d862a5b7044fe386b6a8b5ccc828f3b351f5ca74
Farras Rayhand <farrasrayhand@gmail.com>
2021-12-22 16:00:51 +0800

    beta push 3

--- app/src/main/java/com/laskarlembur/project/HomeActivity.kt
+++ app/src/main/java/com/laskarlembur/project/HomeActivity.kt
@@ -3,12 +3,24 @@ package com.laskarlembur.project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var logoutbutton : Button
+    private lateinit var toolbar: Toolbar
+    private lateinit var nav_bottom : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +28,37 @@ class HomeActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        logoutbutton = findViewById(R.id.btnLogout)
-        logoutbutton.setOnClickListener{
-            auth.signOut()
-            Intent(this@HomeActivity, LoginActivity::class.java).also {
-                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(it)
-            }
-        }
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_home, R.id.nav_profile
+        ).build()
+
+        nav_bottom = findViewById(R.id.nav_bottom)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_bottom.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_logout ->{
+                auth.signOut()
+                Intent(this@HomeActivity, LoginActivity::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
+                return true
+            }
+            else -> return true
+        }
     }
 }
