cca245592456cd28f59abf9c803bf061cfc03b22
Farras Rayhand <farrasrayhand@gmail.com>
2021-12-29 09:29:45 +0800

    its done?

--- app/src/main/java/com/laskarlembur/project/AboutActivity.kt
+++ app/src/main/java/com/laskarlembur/project/AboutActivity.kt
@@ -1,11 +1,59 @@
 package com.laskarlembur.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var logoutbutton : Button
+    private lateinit var toolbar: Toolbar
+    private lateinit var nav_bottom : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        auth = FirebaseAuth.getInstance()
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.menu_home) {
+            Intent(this@AboutActivity, HomeActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+            }
+            return true
+        }
+        if (id == R.id.menu_logout) {
+            auth.signOut()
+            Intent(this@AboutActivity, LoginActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+            }
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
